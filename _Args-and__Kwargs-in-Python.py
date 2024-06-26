6a07b4afa422f9125426163a65b1419c7933223d
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-06-26 23:19:50 +0500

    *Args Positional Arguments, and **Kwargs Keyword Arguments in Python

---
+++ _Args-and__Kwargs-in-Python.py
@@ -0,0 +1,16 @@
+# *args:     Many Positional Arguments, it creates Tuples for storing data
+# **kwargs:  Many Keyword Arguments, it creates Dictionary for storing data
+
+# *Args
+def myFun1(*args):
+    return
+
+myFun1(1,2,3,4,5)
+
+
+
+# **Kwargs
+def myFun2(**kwargs):
+    return
+
+myFun2(jawad=32,adan=45, danial =34)
